89bc1c46fe867d7a3e61574e66966a00a9c08ef2
Asimsaeed111 <161206949+Asimsaeed111@users.noreply.github.com>
2024-03-05 14:15:09 +0500

    Day 1  coding Challange

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+var myname = "Asim saeed";
+console.log(myname);
+console.log(`Hello ${myname} would you like to learn some typescript today?`);
+console.log(myname.toUpperCase());
+console.log(myname.toLowerCase());
+console.log(myname.charAt(0).toUpperCase() + myname.slice(1).toLowerCase());
---
+++ main.ts
@@ -0,0 +1,6 @@
+var myname: string = "Asim saeed";
+console.log(myname)
+console.log(`Hello ${myname} would you like to learn some typescript today?`)
+console.log(myname.toUpperCase());
+console.log(myname.toLowerCase());
+console.log(myname.charAt(0).toUpperCase() + myname.slice(1).toLowerCase());
